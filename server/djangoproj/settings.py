06b0163854cea15220ca4aaef4bf7eb646247f7c
pedro <pedro.accdev@gmail.com>
2026-07-20 16:52:43 -0400

    Add CSRF trusted origins and allowed hosts for Theia proxy

--- server/djangoproj/settings.py
+++ server/djangoproj/settings.py
@@ -21,9 +21,12 @@ SECRET_KEY =\
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
-CSRF_TRUSTED_ORIGINS = []
+CSRF_TRUSTED_ORIGINS = [
+    'https://scontatopedr-8000.theiadockernext-1-labs-prod-theiak8s-4-tor01.proxy.cognitiveclass.ai',
+    'https://scontatopedr-3000.theiadockernext-1-labs-prod-theiak8s-4-tor01.proxy.cognitiveclass.ai',
+]
 
 REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': [],
